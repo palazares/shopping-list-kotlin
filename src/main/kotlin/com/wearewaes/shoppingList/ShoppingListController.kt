9be31674715c4de230e27e37f38c6ac45e12db44
Denis Sigaev <palazares@gmail.com>
2020-02-27 16:22:19 +0100

    JavaDocs were added

--- src/main/kotlin/com/wearewaes/shoppingList/ShoppingListController.kt
+++ src/main/kotlin/com/wearewaes/shoppingList/ShoppingListController.kt
@@ -9,6 +9,14 @@ import reactor.core.publisher.Mono
 @RestController
 @RequestMapping("api")
 class ShoppingListController(private val service: ShoppingListService) {
+    /**
+     * Add an item to ShoppingList. Creates a new list if it doesn't exist
+     *
+     * @param listId id of a ShoppingList
+     * @param description description of new ListItem
+     * @param quantity optional with default value 1. Quantity of a ListItem being added
+     * @return Updated or created ShoppingList
+     */
     @PostMapping("{listId}/add")
     fun addItem(@PathVariable listId: String,
                 @RequestParam("description") description: String,
@@ -16,16 +24,37 @@ class ShoppingListController(private val service: ShoppingListService) {
         return service.addItem(listId, description, quantity)
     }
 
+    /**
+     * Deletes an item from ShoppingList. Does nothing if no matching ids were found
+     *
+     * @param listId id of a ShoppingList
+     * @param itemId id of a ListItem
+     * @return updated ShoppingList or Mono.empty()
+     */
     @PutMapping("{listId}/delete/{itemId}")
     fun deleteItem(@PathVariable listId: String, @PathVariable itemId: Long): Mono<ShoppingList> {
         return service.deleteItem(listId, itemId)
     }
 
+    /**
+     * Deletes all items from ShoppingList. Does nothing if no matching id were found
+     *
+     * @param listId id of a ShoppingList
+     * @return updated ShoppingList or Mono.empty()
+     */
     @PutMapping("{listId}/deleteAll")
     fun deleteAll(@PathVariable listId: String): Mono<ShoppingList> {
         return service.deleteAll(listId)
     }
 
+    /**
+     * Updates an item quantity in a ShoppingList. Does nothing if no matching ids were found
+     *
+     * @param listId id of a ShoppingList
+     * @param itemId id of a ListItem
+     * @param quantity new quantity. Should be bigger than 0, otherwise Mono.error() returned
+     * @return updated ShoppingList or Mono.empty(). Mono.error() in case of non-positive quantity
+     */
     @PutMapping("{listId}/updateQuantity")
     fun updateQuantity(@PathVariable listId: String,
                        @RequestParam("itemId") itemId: Long,
@@ -33,6 +62,13 @@ class ShoppingListController(private val service: ShoppingListService) {
         return service.updateQuantity(listId, itemId, quantity)
     }
 
+    /**
+     * Finds ShoppingList and returns it sorted.
+     *
+     * @param listId id of a ShoppingList
+     * @param sortBy optional. position by default. property name of a ListItem used for sorting
+     * @return ShoppingList or Mono.empty()
+     */
     @GetMapping("{listId}")
     fun getList(@PathVariable listId: String,
                 @RequestParam(value = "sortBy", required = false, defaultValue = "position") sortBy: String): Mono<ShoppingList> {
--- src/main/kotlin/com/wearewaes/shoppingList/ShoppingListService.kt
+++ src/main/kotlin/com/wearewaes/shoppingList/ShoppingListService.kt
@@ -17,6 +17,14 @@ interface ShoppingListRepository : ReactiveCrudRepository<ShoppingList, String>
 class ShoppingListService(private val repo: ShoppingListRepository) {
     private val logger = KotlinLogging.logger {}
 
+    /**
+     * Add an item to ShoppingList. Creates a new list if it doesn't exist
+     *
+     * @param listId id of a ShoppingList
+     * @param description description of new ListItem
+     * @param quantity quantity of new ListItem
+     * @return Updated or created ShoppingList
+     */
     fun addItem(listId: String, description: String, quantity: Int): Mono<ShoppingList> =
             findAndUpdate(listId) { l ->
                 l.defaultIfEmpty(ShoppingList(listId))
@@ -26,14 +34,35 @@ class ShoppingListService(private val repo: ShoppingListRepository) {
                         }
             }.doOnError { logger.debug("Error during adding an item $description to list $listId: ", it) }
 
+    /**
+     * Deletes an item from ShoppingList. Does nothing if no matching ids were found
+     *
+     * @param listId id of a ShoppingList
+     * @param itemId id of a ListItem
+     * @return updated ShoppingList or Mono.empty()
+     */
     fun deleteItem(listId: String, itemId: Long): Mono<ShoppingList> =
             findAndUpdate(listId) { l -> l.map { it.apply { this.list.removeIf { i -> i.id == itemId } } } }
                     .doOnError { logger.debug("Error during deleting an item $itemId from list $listId: : ", it) }
 
+    /**
+     * Deletes all items from ShoppingList. Does nothing if no matching id were found
+     *
+     * @param listId id of a ShoppingList
+     * @return updated ShoppingList or Mono.empty()
+     */
     fun deleteAll(listId: String): Mono<ShoppingList> =
             findAndUpdate(listId) { l -> l.map { it.apply { this.list.clear() } } }
                     .doOnError { logger.debug("Error during deleting all items from list $listId: : ", it) }
 
+    /**
+     * Updates an item quantity in a ShoppingList. Does nothing if no matching ids were found
+     *
+     * @param listId id of a ShoppingList
+     * @param itemId id of a ListItem
+     * @param quantity new quantity. Should be bigger than 0, otherwise Mono.error() returned
+     * @return updated ShoppingList or Mono.empty(). Mono.error() in case of non-positive quantity
+     */
     fun updateQuantity(listId: String, itemId: Long, quantity: Int): Mono<ShoppingList> =
             findAndUpdate(listId) { l ->
                 l.map {
@@ -42,6 +71,13 @@ class ShoppingListService(private val repo: ShoppingListRepository) {
                 }
             }.doOnError { logger.debug("Error during updating quantity for item $itemId in list $listId: : ", it) }
 
+    /**
+     * Finds ShoppingList and returns it sorted.
+     *
+     * @param listId id of a ShoppingList
+     * @param sortBy property name of a ListItem used for sorting
+     * @return ShoppingList or Mono.empty()
+     */
     fun getList(listId: String, sortBy: String): Mono<ShoppingList> =
             repo.findById(listId)
                     .map {
